3a8cb7845aed117335532d23713054cd0fae05ef
Rodrigo Jaramillo <rodrigojaramilloga@gmail.com>
2025-09-03 22:57:41 -0500

    User Token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -32,7 +32,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password_digest)
+    params.require(:user).permit(:email, :password)
   end
 
   def set_user
